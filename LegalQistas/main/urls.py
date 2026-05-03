490dcfe9f5be79270d4aee4b2eed4056c6a05417
Saleh <salehalswat2@gmail.com>
2026-05-03 22:38:58 +0300

    Update base & urls & views and Create Lawyers, about, contact and services pages

--- LegalQistas/main/urls.py
+++ LegalQistas/main/urls.py
@@ -5,5 +5,10 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home_view, name='home_view'),
+    path('services/', views.services_view, name='services_view'),
+    path('lawyers/', views.lawyers_view, name='lawyers_view'),
+    path('about/', views.about_view, name='about_view'),
+    path('contact/', views.contact_view, name='contact_view'),
+    path('mode/<mode>/', views.mode_view, name="mode_view"),
     
 ]
