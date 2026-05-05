f8ae51f0bcc353ea0c54bc66704c8497905d9931
M7mdQash <sil.qash@protonmail.com>
2026-05-05 09:39:38 +0300

    email fix fr now

--- LegalQistas/LegalQistas/settings.py
+++ LegalQistas/LegalQistas/settings.py
@@ -173,6 +173,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@legalqistas.com')
+EMAIL_TIMEOUT = config('EMAIL_TIMEOUT', default=5, cast=int)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
